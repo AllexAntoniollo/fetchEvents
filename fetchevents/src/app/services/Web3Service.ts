cfbc01f87d39bbcd9fe8e1e66f741354612a3446
Sosia <sosia24@proton.me>
2025-12-04 08:13:14 -0300

    change event

--- fetchevents/src/app/services/Web3Service.ts
+++ fetchevents/src/app/services/Web3Service.ts
@@ -2,13 +2,10 @@ import { ethers } from "ethers";
 
 const RPC = "https://arb1.arbitrum.io/rpc";
 const POSITION_MANAGER = "0xC36442b4a4522E871399CD717aBDD847Ab11FE88";
-const CONTRACT_ADDRESS = "0x1D262425dc046b8bb26B4DB4f4Cd754804208049";
-const USDC = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
-
-import abiContract from "./contract.abi.json";
 
 const abi = [
   "event DecreaseLiquidity(uint256 indexed tokenId, uint128 liquidity, uint256 amount0, uint256 amount1)",
+  "event IncreaseLiquidity(uint256 indexed tokenId, uint128 liquidity, uint256 amount0, uint256 amount1)",
 ];
 
 export async function valuePaid(fromBlock: number, toBlock: number) {
@@ -31,24 +28,22 @@ export async function valuePaid(fromBlock: number, toBlock: number) {
 
 export async function valueReceived(fromBlock: number, toBlock: number) {
   const provider = new ethers.JsonRpcProvider(RPC);
+  const contract = new ethers.Contract(POSITION_MANAGER, abi, provider);
 
-  const iface = new ethers.Interface(abiContract);
+  const eventFilter = contract.filters.IncreaseLiquidity(4906534);
 
-  const eventTopic = ethers.id("Transfer(address,address,uint256)");
-  const toTopic = ethers.zeroPadValue(CONTRACT_ADDRESS, 32);
+  const logs = await contract.queryFilter(eventFilter, fromBlock, toBlock);
 
-  const logs = await provider.getLogs({
-    address: USDC,
-    fromBlock: fromBlock,
-    toBlock: toBlock,
-    topics: [eventTopic, null, toTopic],
-  });
-
-  let total = BigInt(0);
+  let totalAmount1 = BigInt(0);
 
   for (const log of logs) {
-    const decoded = iface.decodeEventLog("Transfer", log.data, log.topics);
-    total += decoded.value;
+    if (log instanceof ethers.EventLog) {
+      totalAmount1 += log.args.amount1;
+    }
   }
-  return { qtd: logs.length, valueReceived: total / BigInt(10 ** 6) };
+
+  return {
+    qtd: logs.length,
+    valueReceived: totalAmount1 / BigInt(10 ** 6),
+  };
 }
